ac20417bbb629ec3739b8afca7c76a43aecee3a8
nazneen.rahman <itsnazn33n@gmail.com>
2025-01-16 14:46:05 +0600

    Complete the encapsulation feature

--- src/main/java/com/oop/Main.java
+++ src/main/java/com/oop/Main.java
@@ -1,25 +1,29 @@
 package main.java.com.oop;
 
-import main.java.com.oop.model.Contrator;
-import main.java.com.oop.model.Employee;
-import main.java.com.oop.model.PermanentEmployee;
-import main.java.com.oop.model.Student;
+import main.java.com.oop.model.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        //Object
         Student student = new Student("kareem", "ABC");
 //        System.out.println(student.getName());
 //        System.out.println(student.getCollege());
         student.setName("bareem");
+        //Abstraction
 
         Employee contrator = new Contrator("Nazneen", 10, 9);
         Employee contratorBubu = new Contrator("Bubu", 899, 999);
         Employee permanentEmployee = new PermanentEmployee("Nazneen", 50);
-        System.out.println("Contrator's Salary: " +contrator.calculateSalary());
-        System.out.println("Contrator's Name: " +contrator.getName());
-        System.out.println("ContratorBubu's Salary: " +contratorBubu.calculateSalary());
-        System.out.println("Permanent Employee's Salary: " +permanentEmployee.calculateSalary());
+//        System.out.println("Contrator's Salary: " +contrator.calculateSalary());
+//        System.out.println("Contrator's Name: " +contrator.getName());
+//        System.out.println("ContratorBubu's Salary: " +contratorBubu.calculateSalary());
+//        System.out.println("Permanent Employee's Salary: " +permanentEmployee.calculateSalary());
+
+        //Encapsulation
+        Person person = new Person();
+        person.setName("Nazneen Rahman");;
+        System.out.println("Person 1 - " + person.getId() + " : " + person.getName());
     }
 
 }
